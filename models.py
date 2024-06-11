e8cde1f6efcf68bc243e9285aba7f8ec4e88ff2d
jangam yashwanth raj <77779431+jyashwanthraj@users.noreply.github.com>
2024-06-12 01:18:12 +0530

    Update models.py

--- models.py
+++ models.py
@@ -1,10 +1,9 @@
 # models.py
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import UserMixin
 
 db = SQLAlchemy()
 
-class User(UserMixin, db.Model):
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(150), unique=True, nullable=False)
     password = db.Column(db.String(150), nullable=False)
